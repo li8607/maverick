3d730ca3e7e7b4417c7484ae4fa54d604ed1873e
li8607 <675669728@qq.com>
2017-10-18 07:47:05 +0800

    优化图片加载逻辑。

--- app/src/main/java/com/maverick/adapter/holder/SisterImageHolder.java
+++ app/src/main/java/com/maverick/adapter/holder/SisterImageHolder.java
@@ -2,13 +2,17 @@ package com.maverick.adapter.holder;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.View;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.maverick.DetailActivity;
 import com.maverick.R;
 import com.maverick.bean.BigImgInfo;
 import com.maverick.bean.SisterInfo;
-import com.maverick.util.GlideUtil;
 import com.maverick.weight.RatioImageView;
 
 /**
@@ -19,6 +23,7 @@ public class SisterImageHolder extends SisterTextHolder {
 
     private SisterInfo mSisterInfo;
     private final RatioImageView image;
+    private final View image_text;
 
     public SisterImageHolder(View itemView) {
         super(itemView);
@@ -26,19 +31,61 @@ public class SisterImageHolder extends SisterTextHolder {
         image.setOriginalSize(16, 9);
 
         image.setOnClickListener(this);
+
+        image_text = itemView.findViewById(R.id.image_text);
     }
 
     public void bindData(Context context, SisterInfo sisterInfo) {
         super.bindData(context, sisterInfo);
         this.mSisterInfo = sisterInfo;
-        GlideUtil.loadImage(context, mSisterInfo.getImage2(), image);
-//        Glide.with(context).load(mSisterInfo.getImage2()).priority(Priority.HIGH).skipMemoryCache(true).error(R.mipmap.fail).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(image).getSize(new SizeReadyCallback() {
-//            @Override
-//            public void onSizeReady(int width, int height) {
-//                Log.e(TAG, "width = " + width);
-//                Log.e(TAG, "height = " + height);
-//            }
-//        });
+//        GlideUtil.loadImage(context, mSisterInfo.getImage2(), image);
+        Glide.with(context).load(mSisterInfo.getImage2()).asBitmap().into(new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                Log.e(TAG, "resource.getWidth() = " + resource.getWidth());
+                Log.e(TAG, "resource.getHeight() = " + resource.getHeight());
+                if(resource.getHeight() / resource.getWidth() > 3){
+                    image_text.setVisibility(View.VISIBLE);
+                }else {
+                    image_text.setVisibility(View.INVISIBLE);
+                }
+
+                image.setImageBitmap(ImageCrop(resource, true));
+            }
+        });
+    }
+
+    /**
+     * 按正方形裁切图片
+     */
+    public static Bitmap ImageCrop(Bitmap bitmap, boolean isRecycled)
+    {
+
+        if (bitmap == null)
+        {
+            return null;
+        }
+
+        int w = bitmap.getWidth(); // 得到图片的宽，高
+        int h = bitmap.getHeight();
+
+        int wh = w > h ? h : w;// 裁切后所取的正方形区域边长
+
+        int retX = w > h ? (w - h) / 2 : 0;// 基于原图，取正方形左上角x坐标
+        int retY = w > h ? 0 : (h - w) / 2;
+
+        Bitmap bmp = Bitmap.createBitmap(bitmap, retX, retY, wh, wh, null,
+                false);
+        if (isRecycled && bitmap != null && !bitmap.equals(bmp)
+                && !bitmap.isRecycled())
+        {
+            bitmap.recycle();
+            bitmap = null;
+        }
+
+        // 下面这句是关键
+        return bmp;// Bitmap.createBitmap(bitmap, retX, retY, wh, wh, null,
+        // false);
     }
 
 
