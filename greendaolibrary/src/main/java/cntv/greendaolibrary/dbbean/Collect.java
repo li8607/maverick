8cfb6eab5d07515a8d7e98306a32afc9c5c75d65
li8607 <675669728@qq.com>
2017-10-20 18:23:24 +0800

    优化收藏代码。

--- greendaolibrary/src/main/java/cntv/greendaolibrary/dbbean/Collect.java
+++ greendaolibrary/src/main/java/cntv/greendaolibrary/dbbean/Collect.java
@@ -11,86 +11,145 @@ import java.io.Serializable;
  * Created by limingfei on 2017/9/27.
  */
 @Entity
-public class Collect implements Serializable{
+public class Collect implements Serializable {
     @Id
-    @Property(nameInDb = "COLLECTMAJORKEY")
-    private String collectMajorKey;
+    private Long id;
+
     @Property(nameInDb = "COLLECTIMAGE")
     private String collectImage;
+
     @Property(nameInDb = "COLLECTNAME")
     private String collectName;
+
     @Property(nameInDb = "COLLECTCT")
-    private String collectCT;
+    private String collectCT;           //网络数据显示的时间
+
+    @Property(nameInDb = "COLLECTURL")
+    private String collectUrl;
+
+    @Property(nameInDb = "COLLECTUSERID")
+    private String collectUserId;
+
     @Property(nameInDb = "COLLECTTIME") //收藏时间
     private long collectTime;
+
+    @Property(nameInDb = "COLLECTID")
+    private String collectId;
+
     @Property(nameInDb = "COLLECTITEMTYPE")
-    private String collectItemType;  // 1.文本；2.图片；3.动图;
+    private String collectItemType;  // 1.文本；2.图片；3.动图;  大分类下的小分类
+
     @Property(nameInDb = "COLLECTTYPE")
     private String collectType;  // 1.笑话；2.美女
+
     private boolean check;
+
     public String getCollectType() {
         return this.collectType;
     }
+
     public void setCollectType(String collectType) {
         this.collectType = collectType;
     }
+
     public String getCollectItemType() {
         return this.collectItemType;
     }
+
     public void setCollectItemType(String collectItemType) {
         this.collectItemType = collectItemType;
     }
+
     public long getCollectTime() {
         return this.collectTime;
     }
+
     public void setCollectTime(long collectTime) {
         this.collectTime = collectTime;
     }
+
     public String getCollectName() {
         return this.collectName;
     }
+
     public void setCollectName(String collectName) {
         this.collectName = collectName;
     }
+
     public String getCollectImage() {
         return this.collectImage;
     }
+
     public void setCollectImage(String collectImage) {
         this.collectImage = collectImage;
     }
 
-    public String getCollectMajorKey() {
-        return this.collectMajorKey;
-    }
-    public void setCollectMajorKey(String collectMajorKey) {
-        this.collectMajorKey = collectMajorKey;
-    }
     public String getCollectCT() {
         return this.collectCT;
     }
+
     public void setCollectCT(String collectCT) {
         this.collectCT = collectCT;
     }
+
     public boolean getCheck() {
         return this.check;
     }
+
     public void setCheck(boolean check) {
         this.check = check;
     }
 
-    @Generated(hash = 855346186)
-    public Collect(String collectMajorKey, String collectImage, String collectName,
-            String collectCT, long collectTime, String collectItemType,
+    public String getCollectUserId() {
+        return this.collectUserId;
+    }
+
+    public void setCollectUserId(String collectUserId) {
+        this.collectUserId = collectUserId;
+    }
+
+    public String getCollectUrl() {
+        return this.collectUrl;
+    }
+
+    public void setCollectUrl(String collectUrl) {
+        this.collectUrl = collectUrl;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCollectId() {
+        return this.collectId;
+    }
+
+    public void setCollectId(String collectId) {
+        this.collectId = collectId;
+    }
+
+    @Generated(hash = 1460380355)
+    public Collect(Long id, String collectImage, String collectName,
+            String collectCT, String collectUrl, String collectUserId,
+            long collectTime, String collectId, String collectItemType,
             String collectType, boolean check) {
-        this.collectMajorKey = collectMajorKey;
+        this.id = id;
         this.collectImage = collectImage;
         this.collectName = collectName;
         this.collectCT = collectCT;
+        this.collectUrl = collectUrl;
+        this.collectUserId = collectUserId;
         this.collectTime = collectTime;
+        this.collectId = collectId;
         this.collectItemType = collectItemType;
         this.collectType = collectType;
         this.check = check;
     }
+
     @Generated(hash = 1726975718)
     public Collect() {
     }
