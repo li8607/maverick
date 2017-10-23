4b33f36ae6a376c5e1ebe1a16ee730b04ecef861
li8607 <675669728@qq.com>
2017-10-23 15:27:19 +0800

    优化代码。

--- app/src/main/java/com/maverick/MainApp.java
+++ app/src/main/java/com/maverick/MainApp.java
@@ -28,7 +28,7 @@ public class MainApp extends Application {
         mContext = getApplicationContext();
 
         //开启debug模式，方便定位错误，具体错误检查方式可以查看http://dev.umeng.com/social/android/quick-integration的报错必看，正式发布，请关闭该模式
-        Config.DEBUG = true;
+        Config.DEBUG = false;
         QueuedWork.isUseThreadPool = false;
         UMShareAPI.get(this);
 
