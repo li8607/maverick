b8e1c7ae4f497389bd5f600e9bf7facac49d7171
li8607 <675669728@qq.com>
2017-12-01 18:15:15 +0800

    添加梨视频详情。

---
+++ themelibrary/src/main/java/cntv/themelibrary/Themeable.java
@@ -0,0 +1,9 @@
+package cntv.themelibrary;
+
+/**
+ * Created by darken (darken@darken.eu) on 04.03.2017.
+ * Views that can react to theme changes should implement this.
+ */
+public interface Themeable {
+    void refreshTheme(ThemeHelper themeHelper);
+}
