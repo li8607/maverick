dd96a7f49fb10ea8bb506fe05542f4765fd955e9
li8607 <675669728@qq.com>
2018-02-11 15:32:21 +0800

    优化主题。

--- app/src/main/java/com/maverick/adapter/MenuDialogAdapter.java
+++ app/src/main/java/com/maverick/adapter/MenuDialogAdapter.java
@@ -1,16 +1,19 @@
 package com.maverick.adapter;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.maverick.R;
 import com.maverick.bean.MenuItemInfo;
 import com.maverick.type.MenuType;
+import com.maverick.util.DensityUtil;
 
 import java.util.List;
 
@@ -46,14 +49,14 @@ public class MenuDialogAdapter extends RecyclerView.Adapter {
 
     public class ShareViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private final ImageView image;
+        private final AppCompatImageView image;
         private final TextView title;
         private MenuItemInfo mInfo;
 
         public ShareViewHolder(View itemView) {
             super(itemView);
-            image = (ImageView) itemView.findViewById(R.id.image);
-            title = (TextView) itemView.findViewById(R.id.title);
+            image = itemView.findViewById(R.id.image);
+            title = itemView.findViewById(R.id.title);
             itemView.setOnClickListener(this);
         }
 
@@ -62,22 +65,30 @@ public class MenuDialogAdapter extends RecyclerView.Adapter {
             switch (menuItemInfo.getMenuType()) {
                 case MenuType.SHARE_WEIXIN:
                     image.setImageResource(R.drawable.umeng_socialize_wechat);
+                    image.setPadding(0, 0, 0, 0);
                     title.setText(itemView.getContext().getString(R.string.share_wechat));
                     break;
                 case MenuType.SHARE_WEIXIN_CIRCLE:
                     image.setImageResource(R.drawable.umeng_socialize_wxcircle);
+                    image.setPadding(0, 0, 0, 0);
                     title.setText(itemView.getContext().getString(R.string.share_wxcircle));
                     break;
                 case MenuType.SHARE_SINA:
                     image.setImageResource(R.drawable.umeng_socialize_sina);
+                    image.setPadding(0, 0, 0, 0);
                     title.setText(itemView.getContext().getString(R.string.share_sina));
                     break;
                 case MenuType.SHARE_QZONE:
                     image.setImageResource(R.drawable.umeng_socialize_qzone);
+                    image.setPadding(0, 0, 0, 0);
                     title.setText(itemView.getContext().getString(R.string.share_qzone));
                     break;
                 case MenuType.SEND_COLLENT:
-                    image.setImageResource(R.drawable.collect_selector);
+                    image.setImageResource(R.drawable.ic_favorite_black_24dp);
+                    image.setPadding(DensityUtil.dip2px(mContext, 10), DensityUtil.dip2px(mContext, 10), DensityUtil.dip2px(mContext, 10), DensityUtil.dip2px(mContext, 10));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        image.setImageTintList(ContextCompat.getColorStateList(mContext, R.color.selector_radiobutton_text_color_main));
+                    }
                     updateCollectUI(menuItemInfo);
                     break;
             }
